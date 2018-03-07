84cceb6894b80ec8a848c5641cdc0e86fb5039cf
bvasilop <bvasilop@gmail.com>
2018-03-06 22:22:14 -0800

    cleaned up code with beautify

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,32 @@
 $(function() {
+  /*======= Skillset Section =======*/
 
+  $('.level-bar-inner').css('width', '0');
 
-    /*======= Skillset Section =======*/
+  $(window).on('load', function() {
+    $('.level-bar-inner').each(function() {
+      var itemWidth = $(this).data('level');
 
-    $('.level-bar-inner').css('width', '0');
-
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-
-            var itemWidth = $(this).data('level');
-
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-
-        });
-
+      $(this).animate(
+        {
+          width: itemWidth,
+        },
+        800,
+      );
     });
+  });
 
-    /* Bootstrap Tooltip for Skillset Section*/
-    $('.level-label').tooltip();
+  /* Bootstrap Tooltip for Skillset Section*/
+  $('.level-label').tooltip();
 
-
-
-
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar
+  /* Github Calendar - https://github.com/IonicaBizau/github-calendar
     GitHubCalendar("#github-graph", "bvasilop");*/
 
+  /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+  GitHubActivity.feed({ username: 'bvasilop', selector: '#ghfeed' });
 
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "bvasilop", selector: "#ghfeed" });
-
-    //Scroll Reveal
-
-
+  //Scroll Reveal
 });
-
 
 window.sr = ScrollReveal({ reset: true });
 sr.reveal('.header', { duration: 900 });
@@ -47,6 +37,3 @@ sr.reveal('.header .profile-content .desc', { duration: 2500 });
 sr.reveal('.section-inner', { duration: 700 });
 sr.reveal('.skills aside section', { duration: 2000 });
 sr.reveal('.footer', { duration: 700 });
-
-
-
