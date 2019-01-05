d633e05c2ec5d65c87ee74fb5767d360030288b0
bvasilop <bvasilop@gmail.com>
2019-01-04 19:15:04 -0800

    feature: added service worker with service worker cache. Rewmoved node modules

---
+++ assets/js/sw.js
@@ -0,0 +1,165 @@
+
+// const currentCacheVersion = 'billvas-portfolio-v1';
+// const imagesToCache = [
+//   '/assets/images_comp/angular-image-1600_large_2x.png',
+//   '/assets/images_comp/coffee-1600_large_2x.png',
+//   '/assets/images_comp/flickr-1600_large_2x.png',
+//   '/assets/images_comp/-1600_large_2x.png',
+//   '/assets/images_comp/github-chart-1600_large_2x.png',
+//   '/assets/images_comp/GrowWithGoogle-1600_large_2x.png',
+//   '/assets/images_comp/headshot-1600_large_2x.png',
+//   '/assets/images_comp/ilios-1600_large_2x.png',
+//   '/assets/images_comp/rainier-1600_large_2x.png',
+//   '/assets/images_comp/science-journal-1600_large_2x.png',
+//   '/assets/images_comp/tangletown-wordpress-1600_large_2x.png',
+//   '/assets/images_comp/vue-js-1600_large_2x.png',
+//   '/assets/images_comp/vue-pwa-1600_large_2x.png',
+// ];
+
+// const resourcesToCache = [
+//   '/',
+//   '/index.html',
+//   '/assets/css/styles.css',
+//   '/assets/js/main.js',
+//   ...imagesToCache
+// ];
+
+// // Install SW
+// self.addEventListener('install', event => {
+//   // Cache the resources
+//   // instead of aggressively caching all images, since we have 6 variations of each image, I'd only cache them as they come as requests in the SW's fetch event
+//   event.waitUntil(
+//     caches.open(currentCacheVersion).then(cache => {
+//       return cache.addAll(resourcesToCache);
+//     })
+//   );
+// });
+
+// // SW activate
+// self.addEventListener('activate', event => {
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       // Delete all other versions of the 'mws-restaurant' cache except for the current one
+//       return Promise.all(
+//         cacheNames
+//           .filter(cacheName => {
+//             return (
+//               cacheName.startsWith('billvas-portfolio') &&
+//               currentCacheVersion != cacheName
+//             );
+//           })
+//           .map(cacheName => {
+//             return caches.delete(cacheName);
+//           })
+//       );
+//     })
+//   );
+// });
+
+// // SW Fetch
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.open(currentCacheVersion).then(cache => {
+//       return cache.match(event.request).then(response => {
+//         // Return response from cache if one exists
+//         if (response) return response;
+
+//         // Otherwise hit the network
+//         return fetch(event.request).then(netResponse => {
+//           // Only cache images from the app
+//           if (
+//             netResponse.url.includes('.png') ||
+//             netResponse.url.includes('.jpg')
+//           ) {
+//             if (netResponse.url.includes(window.location.origin)) {
+//               cache.put(event.request.url, netResponse.clone());
+//               return netResponse;
+//             }
+//             return;
+//           }
+//           console.log(netResponse);
+//           cache.put(event.request.url, netResponse.clone());
+//           return netResponse;
+//         });
+//       });
+//     })
+//   );
+// });
+
+const imagesToCache = [
+    '/assets/images_comp/angular-image-1600_large_2x.png',
+    '/assets/images_comp/coffee-1600_large_2x.png',
+    '/assets/images_comp/flickr-1600_large_2x.png',
+    '/assets/images_comp/github-chart-1600_large_2x.png',
+    '/assets/images_comp/GrowWithGoogle-1600_large_2x.png',
+    '/assets/images_comp/headshot-1600_large_2x.png',
+    '/assets/images_comp/ilios-1600_large_2x.png',
+    '/assets/images_comp/rainier-1600_large_2x.png',
+    '/assets/images_comp/science-journal-1600_large_2x.png',
+    '/assets/images_comp/tangletown-wordpress-1600_large_2x.png',
+    '/assets/images_comp/vue-js-1600_large_2x.png',
+    '/assets/images_comp/vue-pwa-1600_large_2x.png',
+    '/favicon.ico'
+];
+
+// Cache the application shell
+
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/assets/css/styles.css',
+    '/assets/js/main.js',
+    '/assets/js/sw.js',
+    '/assets/plugins/bootstrap/css/bootstrap.min.css',
+    '/assets/plugins/font-awesome/css/font-awesome.css',
+    '/assets/plugins/github-activity/src/github-activity.css',
+    //'//cdnjs.cloudflare.com/ajax/libs/octicons/2.0.2/octicons.min.css',
+    '/assets/plugins/jquery-1.11.3.min.js',
+    '/assets/plugins/bootstrap/js/bootstrap.min.js',
+    //'//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min.js',
+    '/assets/plugins/github-activity/src/github-activity.js',
+    //'https://unpkg.com/scrollreveal/dist/scrollreveal.min.js',
+    //'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js',
+    //'http://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic',
+    //'http://fonts.googleapis.com/css?family=Montserrat:400,700',
+    //'https://www.googletagmanager.com/gtag/js?id=UA-110221936-1',
+    //'https://platform.linkedin.com/badges/js/profile.js',
+
+    ...imagesToCache
+  ];
+
+  const staticCacheName = 'billvas-portfolio-v1';
+
+  self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+      caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+
+  // Serve files from the cache
+
+  self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request)
+
+        // TODO 4 - Add fetched files to the cache
+
+      }).catch(error => {
+
+        // TODO 6 - Respond with custom offline page
+
+      })
+    );
+  });
