1da71d96c70e3824d8775eabbbaaf831f8217b7d
bvasilop <bvasilop@gmail.com>
2019-10-15 11:36:21 -0700

    formatted gruntifile

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,64 +7,73 @@
 */
 
 module.exports = function(grunt) {
-
-    grunt.initConfig({
-      responsive_images: {
-        dev: {
-          options: {
-            engine: 'im',
-            sizes: [{
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [
+            {
               width: 1600,
               suffix: '_large_2x',
-              quality: 30
-            }]
-          },
+              quality: 30,
+            },
+          ],
+        },
 
-          /*
+        /*
           You don't need to change this part if you don't change
           the directory structure.
           */
-          files: [{
+        files: [
+          {
             expand: true,
             src: ['*.{gif,jpg,png}'],
             cwd: 'assets/images/',
-            dest: 'assets/images_comp/'
-          }]
-        }
-      },
-
-      /* Clear out the images directory if it exists */
-      clean: {
-        dev: {
-          src: ['images'],
-        },
-      },
-
-      /* Generate the images directory if it is missing */
-      mkdir: {
-        dev: {
-          options: {
-            create: ['images']
+            dest: 'assets/images_comp/',
           },
+        ],
+      },
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images'],
         },
       },
+    },
 
-      /* Copy the "fixed" images that don't go through processing into the images/directory */
-      copy: {
-        dev: {
-          files: [{
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [
+          {
             expand: true,
             src: 'images_src/fixed/*.{gif,jpg,png}',
-            dest: 'images/'
-          }]
-        },
+            dest: 'images/',
+          },
+        ],
       },
-    });
+    },
+  });
 
-    grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-mkdir');
-    grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
-
-  };
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', [
+    'clean',
+    'mkdir',
+    'copy',
+    'responsive_images',
+  ]);
+};
