9ad41eb3d7fa66fd63d3176c16d56dc3eed15134
Bill Vasilopoulos <billvasilopoulos@Bills-MBP-2.domain>
2020-03-17 20:20:37 -0700

    corrected service worker cached files. works offline

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,103 +4,103 @@
 
 // Register service worker after the page has been completely loaded
 if ('serviceWorker' in navigator) {
-
-
   window.addEventListener('load', function() {
     navigator.serviceWorker
-      .register('/assets/js/sw.js')
+      .register('sw.js')
       .then(() => console.log('SW is registered!'));
-    //.catch(function() {
+    // .catch(function() {
     //  console.log('Registration failed!');
-
   });
 }
 
-
-
-//Scroll Reveal
+// Scroll Reveal
 window.sr = ScrollReveal({
-  reset: true
+  reset: true,
 });
-//sr.reveal('a', { duration: 1100 });
-//sr.reveal('h2', { duration: 1000 });
-//sr.reveal('p', { duration: 1200 });
-//sr.reveal('h3', { duration: 1000 });
-//sr.reveal('li', { duration: 1000 });
+// sr.reveal('a', { duration: 1100 });
+// sr.reveal('h2', { duration: 1000 });
+// sr.reveal('p', { duration: 1200 });
+// sr.reveal('h3', { duration: 1000 });
+// sr.reveal('li', { duration: 1000 });
 sr.reveal('.header', {
-  duration: 800
+  duration: 800,
 });
-//sr.reveal('.about section', { duration: 1100 });
+// sr.reveal('.about section', { duration: 1100 });
 sr.reveal('.img-circle', {
-  duration: 1800
+  duration: 1800,
 });
 sr.reveal('.header .profile-content .name', {
-  duration: 2100
+  duration: 2100,
 });
 sr.reveal('.header .profile-content .desc', {
-  duration: 2300
+  duration: 2300,
 });
 // sr.reveal('.section-inner', { duration: 600 });
-//sr.reveal('.skills aside section', { duration: 1800 });
+// sr.reveal('.skills aside section', { duration: 1800 });
 // sr.reveal('.footer', { duration: 600 });
 
-/**** auto current year ****/
+/** ** auto current year *** */
 function currentYear() {
   const date = new Date();
-  //console.log(date);
-  let year = date.getFullYear();
-  //console.log(year);
+  // console.log(date);
+  const year = date.getFullYear();
+  // console.log(year);
 
   document.getElementById('currentYear').innerText = year;
 }
 currentYear();
 
 $(function() {
-  /*======= Skillset Section =======*/
+  /*= ====== Skillset Section ======= */
 
   $('.level-bar-inner').css('width', '0');
 
   $(window).on('load', function() {
     $('.level-bar-inner').each(function() {
-      var itemWidth = $(this).data('level');
+      const itemWidth = $(this).data('level');
 
-      $(this).animate({
+      $(this).animate(
+        {
           width: itemWidth,
         },
-        800,
+        800
       );
     });
   });
 
-  /* Bootstrap Tooltip for Skillset Section*/
+  /* Bootstrap Tooltip for Skillset Section */
   $('.level-label').tooltip();
 
   /* Github Calendar - https://github.com/IonicaBizau/github-calendar
-    GitHubCalendar("#github-graph", "bvasilop");*/
+    GitHubCalendar("#github-graph", "bvasilop"); */
 
   /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
   GitHubActivity.feed({
     username: 'bvasilop',
-    selector: '#ghfeed'
+    selector: '#ghfeed',
   });
 
   $('[data-toggle="tooltip"]').tooltip();
   // Added smooth scrolling to all links in navbar + footer link
   $(".navbar a, footer a[href='#home']").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+    if (this.hash !== '') {
       // Prevent default anchor click behavior
       event.preventDefault();
       // Store hash
-      var hash = this.hash;
+      const { hash } = this;
       // Used jQuery's animate() method to add smooth page scroll
       // Used (900) milliseconds for how long it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1000, function() {
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        1000,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
     } // End if
   });
 });
---
+++ sw.js
@@ -0,0 +1,84 @@
+const currentCacheVersion = 'billvas-portfolio-v1';
+const imagesToCache = [
+  '/assets/images/angular-image.webp',
+  '/assets/images/audit-results.webp',
+  '/assets/images/Budgetize.webp',
+  '/assets/images/coffee.webp',
+  '/assets/images/flickr.webp',
+  '/assets/images/GrowWithGoogle.webp',
+  '/assets/images/headshot.webp',
+  '/assets/images/ilios.webp',
+  '/assets/images/nasa-apod.webp',
+  '/assets/images/rainier.webp',
+  '/assets/images/science-journal.webp',
+  '/assets/images/tangletown-wordpress.webp',
+  '/assets/images/vue-js.webp',
+  '/assets/images/vue-pwa.webp',
+  '/assets/images/gatsby-blog.webp',
+  '/assets/images/grace-hopper.webp',
+  '/assets/images/gwg-logo.webp',
+  '/assets/images/IU-logo.webp',
+  '/assets/images/IU-logo2.webp',
+  '/assets/images/react-flashcard.webp',
+  '/assets/images/react-indecision.webp',
+  '/assets/images/react-note.webp',
+  '/assets/images/react-unsplash-app.webp',
+  '/assets/images/SU-logo.webp',
+  '/assets/images/SU-logo2.webp',
+  '/assets/images/udacity-diploma.webp',
+  '/assets/images/youtube.webp',
+  '/favicon.ico',
+];
+
+const filesToCache = [
+  '.',
+  '/assets/css/styles.css',
+  '/assets/js/main.js',
+  '/sw.js',
+  '/assets/plugins/bootstrap/css/bootstrap.min.css',
+  '/assets/plugins/font-awesome/css/font-awesome.css',
+  '/assets/plugins/github-activity/src/github-activity.css',
+  '/assets/plugins/jquery-1.11.3.min.js',
+  '/assets/plugins/bootstrap/js/bootstrap.min.js',
+  '/assets/plugins/github-activity/src/github-activity.js',
+  ...imagesToCache,
+];
+
+const staticCacheName = 'billvas-portfolio-v1';
+
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(currentCacheVersion).then(cache =>
+      cache.match(event.request).then(response => {
+        // Return response from cache if one exists
+        if (response) return response;
+
+        // Otherwise hit the network
+        return fetch(event.request).then(netResponse => {
+          // Only cache images from the app
+          if (
+            netResponse.url.includes('.jpg') ||
+            netResponse.url.includes('.png') ||
+            netResponse.url.includes('.webp')
+          ) {
+            if (netResponse.url.includes(window.location.origin)) {
+              cache.put(event.request.url, netResponse.clone());
+              return netResponse;
+            }
+            return;
+          }
+          console.log(netResponse);
+          cache.put(event.request.url, netResponse.clone());
+          return netResponse;
+        });
+      })
+    )
+  );
+});
